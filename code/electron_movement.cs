4af0d22d0aaf3468e485264c2c18ce9d64e0d9ee
majaja068 <77485425+majaja068@users.noreply.github.com>
2021-02-17 22:19:28 +0800

    Add files via upload

---
+++ code/electron_movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class electron_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.localPosition =transform.localPosition + Vector3.left;
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.localPosition = transform.localPosition + Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.localPosition = transform.localPosition + Vector3.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.localPosition = transform.localPosition + Vector3.back;
+        }
+        if(Input.GetKey(KeyCode.Backspace))
+        {
+            transform.localPosition = new Vector3(10,0,0);
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+
+}
